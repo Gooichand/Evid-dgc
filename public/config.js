5c3adde0c41a51929a1e582d59c5e61352d4bf15
Gopichand Dandimeni <dandimenichand.230105181015@sandipuniversity.edu.in>
2025-12-05 19:09:49 +0530

    Add Pinata API keys for FREE IPFS storage

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 var config = {
     CONTRACT_ADDRESS: 'YOUR_DEPLOYED_CONTRACT_ADDRESS', // FREE Mumbai deployment
-    PINATA_API_KEY: 'YOUR_PINATA_API_KEY', // FREE 1GB storage
-    PINATA_SECRET_KEY: 'YOUR_PINATA_SECRET_KEY', // FREE account
+    PINATA_API_KEY: '524c401de3e5176e6f46', // Your FREE Pinata API Key
+    PINATA_SECRET_KEY: '2a83244231dd404f740d39cae84d2fb2afe2c866a02fd9d7002fc7cf5e1c010b', // Your FREE Pinata Secret
     IPFS_GATEWAY: 'https://gateway.pinata.cloud/ipfs/', // FREE access
     NETWORK_ID: 80001, // Mumbai testnet - completely FREE
     NETWORK_NAME: 'Polygon Mumbai Testnet - FREE'
